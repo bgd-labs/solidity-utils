a50e48aee59de175f6c5e7b984ca98cb9a1c6714
Andrei Kozlov <kyzia.ru@gmail.com>
2022-06-30 10:16:42 +0200

    OwnableWithGuardian contract

---
+++ src/contracts/access-control/OwnableWithGuardian.sol
@@ -0,0 +1,42 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import {IWithGuardian} from './interfaces/IWithGuardian.sol';
+import {Ownable} from '../../lib/openzeppelin-contracts/contracts/access/Ownable.sol';
+
+abstract contract OwnableWithGuardian is Ownable, IWithGuardian {
+  address private _guardian;
+
+  function guardian() public view returns (address) {
+    return _guardian;
+  }
+
+  /// @inheritdoc IWithGuardian
+  function updateGuardian(address newGuardian) external onlyGuardian {
+    _updateGuardian(newGuardian);
+  }
+
+  /**
+   * @dev method to update the guardian
+   * @param newGuardian the new guardian address
+   */
+  function _updateGuardian(address newGuardian) internal {
+    address oldGuardian = _guardian;
+    _guardian = newGuardian;
+    emit GuardianUpdated(oldGuardian, newGuardian);
+  }
+
+  function _checkGuardian() internal view {
+    require(guardian() == _msgSender(), 'ONLY_BY_GUARDIAN');
+  }
+
+  modifier onlyGuardian() {
+    _checkGuardian();
+    _;
+  }
+
+  modifier onlyOwnerOrGuardian() {
+    require(_msgSender() == owner() || _msgSender() == guardian(), 'ONLY_BY_OWNER_OR_GUARDIAN');
+    _;
+  }
+}
