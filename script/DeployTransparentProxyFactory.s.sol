5ee12c4d65d45c2fdc9ee058a7448dde639fb1a3
Andrei Kozlov <kyzia.ru@gmail.com>
2022-07-12 09:13:16 +0200

    fix import format

--- script/DeployTransparentProxyFactory.s.sol
+++ script/DeployTransparentProxyFactory.s.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import {Script} from '../lib/forge-std/src/Script.sol';
+import {Script} from 'forge-std/Script.sol';
 import {TransparentProxyFactory} from '../src/contracts/transparent-proxy/TransparentProxyFactory.sol';
 
 contract Deploy is Script {
