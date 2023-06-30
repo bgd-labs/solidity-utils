63cda6a0a1c7b6c22522c4fd11f1b1e05602e8d6
sendra <polsendra@gmail.com>
2023-06-30 15:42:54 +0200

    fix: added natspec and test to Rescue

--- src/contracts/utils/Rescuable.sol
+++ src/contracts/utils/Rescuable.sol
@@ -5,14 +5,21 @@ import {IERC20} from '../oz-common/interfaces/IERC20.sol';
 import {SafeERC20} from '../oz-common/SafeERC20.sol';
 import {IRescuable} from './interfaces/IRescuable.sol';
 
+/**
+ * @title Rescuable
+ * @author BGD Labs
+ * @notice abstract contract with the methods to rescue tokens from a contract
+ */
 abstract contract Rescuable is IRescuable {
   using SafeERC20 for IERC20;
 
+  /// @notice modifier that checks that caller is allowed address
   modifier onlyRescueGuardian() {
     require(msg.sender == whoCanRescue(), 'ONLY_RESCUE_GUARDIAN');
     _;
   }
 
+  /// @inheritdoc IRescuable
   function emergencyTokenTransfer(
     address erc20Token,
     address to,
@@ -23,6 +30,7 @@ abstract contract Rescuable is IRescuable {
     emit ERC20Rescued(msg.sender, erc20Token, to, amount);
   }
 
+  /// @inheritdoc IRescuable
   function emergencyEtherTransfer(address to, uint256 amount) external onlyRescueGuardian {
     (bool success, ) = to.call{value: amount}(new bytes(0));
     require(success, 'ETH_TRANSFER_FAIL');
@@ -30,5 +38,6 @@ abstract contract Rescuable is IRescuable {
     emit NativeTokensRescued(msg.sender, to, amount);
   }
 
+  /// @inheritdoc IRescuable
   function whoCanRescue() public view virtual returns (address);
 }
