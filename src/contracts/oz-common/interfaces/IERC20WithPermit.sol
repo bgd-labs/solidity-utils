cccbadb4372d072c69ace2290aa94511517d2f29
sendra <polsendra@gmail.com>
2023-08-10 14:31:16 +0200

    fix: overwrite safeERC20 with 4.9 version

--- src/contracts/oz-common/interfaces/IERC20WithPermit.sol
+++ src/contracts/oz-common/interfaces/IERC20WithPermit.sol
@@ -2,6 +2,6 @@
 pragma solidity ^0.8.0;
 
 import {IERC20} from './IERC20.sol';
-import {IERC20Permit} from './draft-IERC20Permit.sol';
+import {IERC20Permit} from './IERC20Permit.sol';
 
 interface IERC20WithPermit is IERC20, IERC20Permit {}
