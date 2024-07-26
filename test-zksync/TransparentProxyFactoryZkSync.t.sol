366f66ec60acd3f53630f454c5f0f00cd85dc056
Harsh Pandey <harshsatishpandey@gmail.com>
2024-07-26 13:08:37 +0530

    chore: update deploy scripts zksync

--- test-zksync/TransparentProxyFactoryZkSync.t.sol
+++ test-zksync/TransparentProxyFactoryZkSync.t.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.24;
 
 import {Test} from 'forge-std/Test.sol';
 import {TransparentProxyFactoryZkSync} from '../src-zksync/contracts/transparent-proxy/TransparentProxyFactoryZkSync.sol';
