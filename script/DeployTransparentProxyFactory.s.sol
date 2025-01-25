a4402881fbb5ef452253dcf4c35599896461bee5
Lukas <ls@bgdlabs.com>
2025-01-25 11:45:21 +0100

    fix: update configs (#62)

--- script/DeployTransparentProxyFactory.s.sol
+++ script/DeployTransparentProxyFactory.s.sol
@@ -4,10 +4,10 @@ pragma solidity ^0.8.0;
 import {Script} from 'forge-std/Script.sol';
 import {TransparentProxyFactory} from '../src/contracts/transparent-proxy/TransparentProxyFactory.sol';
 
-contract Deploy is Script {
+contract DeployTransparentProxyFactory is Script {
   function run() external {
     vm.startBroadcast();
-    new TransparentProxyFactory();
+    new TransparentProxyFactory{salt: 'aave'}();
     vm.stopBroadcast();
   }
 }
