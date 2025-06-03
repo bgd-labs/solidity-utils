21713da2b9a208988f73285ec811de805f6c8012
sendra <polsendra@gmail.com>
2025-06-03 16:51:09 +0200

    fix: fixed test

--- test/ChainHelperTest.t.sol
+++ test/ChainHelperTest.t.sol
@@ -4,9 +4,11 @@ pragma solidity ^0.8.0;
 import 'forge-std/Test.sol';
 import {ChainHelpers} from '../src/contracts/utils/ChainHelpers.sol';
 
+/// forge-config: default.allow_internal_expect_revert = true
 contract TestChainHelpers is Test {
   function test_selectChain_shouldRevert() external {
-    vm.expectRevert();
+    vm.createSelectFork(vm.rpcUrl('optimism'));
+    vm.expectRevert(ChainHelpers.UnknownChainId.selector);
     ChainHelpers.selectChain(vm, 0);
   }
 }
